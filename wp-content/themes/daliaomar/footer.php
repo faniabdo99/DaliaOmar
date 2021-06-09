89ed0fd71cc554fe24eaa1c85087b35c19ce4e94
faniabdo99 <faniabdo99@gmail.com>
2021-06-09 18:35:18 +0200

    Latest

---
+++ wp-content/themes/daliaomar/footer.php
@@ -0,0 +1,12 @@
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-12">DALIA OMAR</div>
+                <div class="col-lg-4 col-12">QUICK LINKS</div>
+                <div class="col-lg-4 col-12">NEWSLETTER FORM</div>
+            </div>
+        </div>
+    </footer>
+    <?php wp_footer();?>
+    </body>
+</html>
--- wp-content/themes/daliaomar/functions.php
+++ wp-content/themes/daliaomar/functions.php
@@ -4,3 +4,10 @@
     This file have all the hooks and actions to the theme, Please be carful when editing it.
     Author: Picturate.Me[https://picturate.me]
 */
+function daliaomar_files(){
+    wp_enqueue_style('dalia_bootstrap_css' , '//cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css' , [] , date('ymds'));
+    wp_enqueue_style('dalia_main_app_css' , get_template_directory_uri().'/assets/css/app.css' , [] , date('ymds'));
+    wp_enqueue_script('dalia_bootstrap_js' , '//cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js' , [] , date('ymds'));
+    wp_enqueue_script('dalia_fontawesome_js' , '//kit.fontawesome.com/aa028fd33c.js"' , [] , date('ymds'));
+}
+add_action('wp_enqueue_scripts','daliaomar_files');
