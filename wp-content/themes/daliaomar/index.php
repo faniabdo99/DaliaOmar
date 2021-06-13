b2863ad05fb118cdbb1389541a5cf7e845e1f2a0
faniabdo99 <faniabdo99@gmail.com>
2021-06-13 12:48:22 +0200

    Latest

--- wp-content/themes/daliaomar/index.php
+++ wp-content/themes/daliaomar/index.php
@@ -25,4 +25,3 @@
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod modi quibusdam magnam! Sapiente mollitia veritatis fugiat, at iusto quas, dicta doloribus culpa laborum modi aut tempore veniam dolorem sint nobis?</p>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod modi quibusdam magnam! Sapiente mollitia veritatis fugiat, at iusto quas, dicta doloribus culpa laborum modi aut tempore veniam dolorem sint nobis?</p>
 <?php get_footer(); ?>
-
