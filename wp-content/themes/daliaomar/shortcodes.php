4dd3fffefe7f759566523dd01aa4e971479b6929
faniabdo99 <faniabdo99@gmail.com>
2021-07-18 02:17:01 +0200

    Latest

--- wp-content/themes/daliaomar/shortcodes.php
+++ wp-content/themes/daliaomar/shortcodes.php
@@ -24,7 +24,7 @@
                     '.get_the_content().'
                     <br>
                     <div class="text-center">
-                        <a href="/book-a-program/?program='.get_post_field( 'post_name', get_the_ID()).'" class="book-now-btn">BOOK NOW</a>
+                        <a href="'.get_the_permalink().'" class="book-now-btn">BOOK NOW</a>
                     </div>
                 </div>';
             }
@@ -36,38 +36,28 @@
     }
     function daliaomar_programs_single(){
         if(isset($_GET['program'])){
-            $ProgramSlug = $_GET['program'];
-            $args = array(
-                'name' => $the_slug,
-                'post_type'   => 'program',
-                'post_status' => 'publish',
-                'numberposts' => 1
-            );
-            $my_posts = get_posts($args);
-            while($my_posts->have_posts()){
-                return '
-                <div class="container">
-                    <div class="row">
-                        <div class="col-4">
-                            <div class="single-program-page-item" style="background-image:url('.get_the_post_thumbnail_url().')">
-                                <img src="'.get_field('color_image').'" />
-                                <div class="single-program-card-content">
-                                    <h3>'.get_the_title().'</h3>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-8 program-content">
-                            <h2>'.get_the_title().'</h2>
-                            '.get_the_content().'
-                            <br>
-                            <div class="text-center">
-                                <a href="/book-a-program/?program='.get_post_field( 'post_name', get_the_ID()).'" class="book-now-btn">BOOK NOW</a>
+            global $post;
+            return '
+            <div class="container">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="single-program-page-item" style="background-image:url('.get_the_post_thumbnail_url().')">
+                            <img src="'.get_field('color_image').'" />
+                            <div class="single-program-card-content">
+                                <h3>'.get_the_title().'</h3>
                             </div>
                         </div>
                     </div>
+                    <div class="col-lg-8 program-content">
+                        <h2>'.get_the_title().'</h2>
+                        '.get_the_content().'
+                        <br>
+                        <div class="text-center">
+                            <a href="/book-a-program/?program='.get_post_field( 'post_name', get_the_ID()).'" class="book-now-btn">BOOK NOW</a>
+                        </div>
+                    </div>
                 </div>
-                ';
-            }
+            </div>';
         }else{
             exit('Please Choose a program');
         }
