90289e00850a89a06b9ab5e7ed8dcdccba7bbdee
faniabdo99 <faniabdo99@gmail.com>
2021-07-21 02:17:59 +0200

    Lattes

--- wp-content/themes/daliaomar/header.php
+++ wp-content/themes/daliaomar/header.php
@@ -2,20 +2,21 @@
 <html lang="en">
 <head>
     <?php wp_head();?>
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
 </head>
 <body>
     <div class="upper-nav">
         <!-- <a class="newsletter-signup" href="#"><i class="fas fa-newspaper"></i> SIGN UP FOR MY NEWSLETTER</a>
         <br> -->
         <a class="logo-img" href="/"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/logo.png" alt="Dalia Omar" title="Dalia Omar" ></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+        </button>
     </div>
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
