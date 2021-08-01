6ba5b1281f0e81b125914dee4a660c79b490b42e
faniabdo99 <faniabdo99@gmail.com>
2021-08-01 13:51:34 +0200

    Latest

--- wp-content/themes/daliaomar/shortcodes.php
+++ wp-content/themes/daliaomar/shortcodes.php
@@ -8,30 +8,32 @@
             'order' => 'ASC'
         ];
         $query = new WP_Query($args);
-        $Programs = '<div class="container"><div class="row">';
+        $Programs = '<div class="container">';
         if($query->have_posts()){
             while($query->have_posts()){
                 $query->the_post();
                 $Programs .= '
-                <div class="col-lg-4 col-12 mb-4">
-                    <div class="single-program-page-item" style="background-image:url('.get_the_post_thumbnail_url().')">
-                        <img src="'.get_field('color_image').'" />
-                        <div class="single-program-card-content text-center text-uppercase">
-                            <h3>'.get_the_title().'</h3>
+                <div class="row align-items-center mb-5 single-program-container">
+                    <div class="col-lg-4 col-12">
+                        <div class="single-program-page-item" style="background-image:url('.get_the_post_thumbnail_url().')">
+                            <img src="'.get_field('color_image').'" />
+                            <div class="single-program-card-content text-center text-uppercase">
+                                <h3>'.get_the_title().'</h3>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-8 col-12 program-content">
-                    <h2>'.get_the_title().'</h2>
-                    <br>
-                    '.get_the_content().'
-                    <br>
-                    <div class="text-center">
-                        <a href="'.get_the_permalink().'" class="book-now-btn">BOOK NOW</a>
+                    <div class="col-lg-8 col-12 program-content">
+                        <h2>'.get_the_title().'</h2>
+                        <br>
+                        '.get_the_content().'
+                        <br>
+                        <div class="text-center">
+                            <a href="'.get_the_permalink().'" class="book-now-btn">BOOK NOW</a>
+                        </div>
                     </div>
                 </div>';
             }
-            $Programs .= '</div></div>';
+            $Programs .= '</div>';
         }else{
             return "There are no programs to show at the moment";
         }
